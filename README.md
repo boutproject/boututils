68c7b6a668d89859ae55640aa586625761038bdd
loeiten <michael.l.magnussen@gmail.com>
2019-05-24 10:20:55 +0200

    pip 0.1.0 commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # boututils
+
+[![Python](https://img.shields.io/badge/python->=3.6-blue.svg)](https://www.python.org/)
+[![pypi package](https://badge.fury.io/py/boututils.svg)](https://pypi.org/project/boututils/)
+[![PEP8](https://img.shields.io/badge/code%20style-PEP8-brightgreen.svg)](https://www.python.org/dev/peps/pep-0008/)
+[![License](https://img.shields.io/badge/license-LGPL--3.0-blue.svg)](https://github.com/CELMA-project/bout_install/blob/master/LICENSE)
+
+pip-package of what was previously found in 
+`BOUT-dev/tools/pylib/boututils`
---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import setuptools
+from pathlib import Path
+
+name = 'boututils'
+root_path = Path(__file__).parent
+init_path = root_path.joinpath(name, '__init__.py')
+readme_path = root_path.joinpath('README.md')
+
+# https://packaging.python.org/guides/single-sourcing-package-version/
+with init_path.open('r') as f:
+    version_file = f.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        version = version_match.group(1)
+    else:
+        raise RuntimeError('Unable to find version string.')
+
+with readme_path.open('r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author='Ben Dudson et al.',
+    description='Python package to install BOUT++ and its dependencies',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://boutproject.github.io',
+    project_urls={
+        "Bug Tracker": "https://github.com/boutproject/boututils/issues/",
+        "Documentation": "http://bout-dev.readthedocs.io/en/latest/",
+        "Source Code": "https://github.com/boutproject/boututils/",
+    },
+    packages=setuptools.find_packages(),
+    keywords=['bout++',
+              'bout',
+              'plasma',
+              'physics',
+              'data-extraction',
+              'data-analysis',
+              'data-visualization'],
+    install_requires=['numpy',
+                      'matplotlib',
+                      'scipy',
+                      'h5py',
+                      'boututils',
+                      'future',
+                      'bunch',
+                      'mayavi',
+                      'netCDF'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        ('License :: OSI Approved :: '
+         'GNU Lesser General Public License v3 or later (LGPLv3+)'),
+        'Operating System :: OS Independent',
+    ],
+)
